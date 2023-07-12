1bfc86919045f75b9ac020b4df092f375200bec7
Kim Abcouwer <kim@aktivate.com>
2023-07-12 16:19:41 -0600

    Create card layout and render cards

---
+++ src/app/components/layouts/CardLayout.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Image from 'next/image';
+
+import placehoderImage from '../../../../public/placeholder.png';
+
+const CardLayout = (props: Card) => {
+  const { brand, name, set, subset, parallel, cardNumber, year, quantity, price, frontImage }
+    = props;
+
+  return (
+    <a href="#" className="group">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+        {frontImage ?
+          <Image
+            src={frontImage}
+            alt={name}
+            priority
+            className="w-full h-auto object-center object-cover group-hover:opacity-75"
+            width={200}
+            height={300}
+          /> :
+          <Image
+            src={placehoderImage}
+            alt={name}
+            priority
+            className="w-full h-auto object-center object-cover group-hover:opacity-75"
+            width={200}
+            height={300}
+          />
+        }
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">SKU: {cardNumber}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">Quantity: {quantity}</p>
+      <p className="mt-1 text-lg font-medium text-gray-900">Price: ${price}</p>
+    </a>
+  );
+};
+
+export default CardLayout;
