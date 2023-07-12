b67d328ff818ac2141212554cd07b648e8a2bf39
Kim Abcouwer <kim@aktivate.com>
2023-07-12 15:17:08 -0600

    Add card type and api call

---
+++ src/app/api/cards/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+
+const DATA_SOURCE_URL = "https://mocki.io/v1/47ed69ba-ce12-4a2a-8853-c5d49ad6a821";
+
+export async function GET() {
+  try {
+    const response = await fetch(DATA_SOURCE_URL);
+    const cards: Card[] = await response.json();
+
+    const ITEMS_PER_PAGE = 10;
+
+    return NextResponse.json({
+      success: true,
+      totalCount: cards.length,
+      pageCount: Math.ceil(cards.length / ITEMS_PER_PAGE),
+      perPage: ITEMS_PER_PAGE,
+      cards: cards,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      error: error.message,
+    });
+  }
+}
---
+++ types.d.ts
@@ -0,0 +1,12 @@
+type Card = {
+  brand: string;
+  name: string;
+  set: string;
+  subset: string;
+  parallel: string;
+  cardNumber: string;
+  year: string;
+  quantity: string;
+  price: string;
+  frontImage: string;
+}
